c96dd3793ba12a56974bc055926b0d6dfb0c05c9
jmg0343 <jmg0343@gmail.com>
2021-05-05 18:57:13 -0500

    add model/route binding

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -15,10 +15,8 @@ class TodosController extends Controller
         return view('todos.index')->with('todos', $todos);
     }
 
-    public function show($todoId)
+    public function show(Todo $todo)
     {
-        $todo = Todo::find($todoId);
-
         return view('todos.show')->with('todo', $todo);
     }
 
@@ -51,14 +49,12 @@ class TodosController extends Controller
         return redirect('/todos');
     }
 
-    public function edit($todoId)
+    public function edit(Todo $todo)
     {
-        $todo = Todo::find($todoId);
-
         return view('todos.edit')->with('todo', $todo);
     }
 
-    public function update($todoId)
+    public function update(Todo $todo)
     {
         $this->validate(request(), [
             'name' => 'required',
@@ -66,8 +62,6 @@ class TodosController extends Controller
         ]);
 
         $data = request()->all();
-
-        $todo = Todo::find($todoId);
 
         $todo->name = $data['name'];
         $todo->description = $data['description'];
@@ -77,10 +71,8 @@ class TodosController extends Controller
         return redirect('/todos');
     }
 
-    public function delete($todoId)
+    public function delete(Todo $todo)
     {
-        $todo = Todo::find($todoId);
-
         $todo->delete();
 
         return redirect('/todos');
